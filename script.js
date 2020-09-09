fc7d2e246c06d6366e4792cdb17fc7b6643e19ee
Kissa0330 <raika4789@gmail.com>
2020-09-09 23:56:20 +0900

    入力周りの調整

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var w = 307, h = 307;
 var outerRadius = w / 2;
 var innerRadius = outerRadius - 60;
 
-var createGradient = function (svg, color1, color2, id) {
+var createGradient = function (svg, color, color1, id) {
 
   var defs = svg.append("defs");
 
@@ -16,12 +16,12 @@ var createGradient = function (svg, color1, color2, id) {
 
   gradient.append("svg:stop")
     .attr("offset", "0%")
-    .attr("stop-color", color1)
+    .attr("stop-color", color)
     .attr("stop-opacity", 1);
 
   gradient.append("svg:stop")
     .attr("offset", "100%")
-    .attr("stop-color", color2)
+    .attr("stop-color", color1)
     .attr("stop-opacity", 1);
 };
 
@@ -74,59 +74,58 @@ var createGroove = function () {
 };
 
 // 時間に応じてタスクを生成
-var createTask = function (startTime, endTime ,random) {
+var createTask = function (startTime, endTime, code) {
   var arcForeground = d3.svg.arc()
     .innerRadius(innerRadius)
     .outerRadius(outerRadius)
-    .startAngle(2 * Math.PI / 12 * startTime)
-    .endAngle(2 * Math.PI / 12 * endTime)
+    .startAngle(2 * Math.PI / 24 * startTime)
+    .endAngle(2 * Math.PI / 24 * endTime)
 
   var pathForeground = svg.append('path')
     .attr({
       d: arcForeground
     })
-    .style("fill", "url(#taskGradient"+random+")");
+    .style("fill", "url(#taskGradient" + code + ")");
 };
 
-
-// var addGradient = function (path, id) {
-//   d3.path.style({
-//     fill: "url('#" + id + "')"
-//   });
-// };
-
+// data
 var Tasks = [
-  { name: 'activity1', startTime: 0, endTime: 3 },
-  { name: 'activity2', startTime: 4, endTime: 6 },
-  { name: 'activity3', startTime: 9, endTime: 11 }
+  { name: 'running', startTime: "2:00", endTime: "3:00" },
+  { name: 'homework', startTime: "5:35", endTime: "8:00" },
+  { name: '買い物', startTime: "23:00", endTime: "24:00" }
 ];
 
 var Gradients = [
   { name: "red", color: "#fe08b5", color1: "#ff1410" },
   { name: "lightBlue", color: "72c3ff", color1: "61cad8" },
   { name: "pink", color: "ff729d", color1: "e86789" },
-  { name: "purple", color: "8872ff", color1: "7861d8" },
   { name: "blue", color: "#65B2FF", color1: "#5F9BE0" },
   { name: "green", color: "#BEEAA6", color1: "#89D568" },
   { name: "orange", color: "#FCB47E", color1: "#DE863C" },
+  { name: "purple", color: "8872ff", color1: "7861d8" },
   { name: "lightPurple", color: "#D48BEF", color1: "#AA37DD" },
-  { name: "emerald", color: "#8BEFEF", color1: "#37DDB0"},
+  { name: "emerald", color: "#8BEFEF", color1: "#37DDB0" },
   { name: "lightRed", color: "#EF8B8B", color1: "#EB5869" }
 ];
 
 createGradient(svg, '#f5f4f4', '#f0efef', 'grooveGradient');
 createGroove();
 
-// TODOグラデーションの処理を追加
-// 色を定着させる処理を追加
+
 for (var i = 0; i < Tasks.length; ++i) {
-  var random = Math.floor(Math.random() * 10);
-  createGradient(svg, Gradients[random].color, Gradients[random].color1, "taskGradient"+random+"");
+  var number = Tasks[i].name.charCodeAt(0);
+  var code = number.toString().split('').pop();
+  
+  var start = Tasks[i].startTime.split(":");
+  var startTime = (start[0] + start[1] * 0.0166) * 0.1;
 
-  createTask(Tasks[i].startTime, Tasks[i].endTime, random);
+  var end = Tasks[i].endTime.split(":");
+  var endTime = (end[0] + end[1] * 0.0166) * 0.1;
+  
+  createGradient(svg, Gradients[code].color, Gradients[code].color1, "taskGradient" + code + "");
 
-  console.log(random)
-  console.log("taskGradient"+random+"");
+  createTask(startTime, endTime, code);
+
+  console.log(code)
+  console.log("taskGradient" + Tasks[i].code + "");
 };
-
-
